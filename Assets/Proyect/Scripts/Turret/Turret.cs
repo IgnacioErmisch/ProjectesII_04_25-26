20baf76c9b6fd4e774142da9476d0af41120472c
NachoErmisch <nachoermisch@gmail.com>
2025-12-11 00:41:49 +0100

    constructores enemigos e interfaces fuera

--- Assets/Proyect/Scripts/Turret/Turret.cs
+++ Assets/Proyect/Scripts/Turret/Turret.cs
@@ -1,59 +1,67 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using UnityEngine.Rendering;
 
 public class Turret : MonoBehaviour
 {
-    [SerializeField] private RadiusDetectionSystem detectionSystem;
+    [Header("Detection Settings")]
     [SerializeField] private Transform detectionPoint;
-    [SerializeField] private Transform playerTarget;
-    [SerializeField] private GameObject turretBullet;
-    [SerializeField] private Transform spawnBullets;
     [SerializeField] private float detectionRadius = 10f;
     [SerializeField] private LayerMask playerLayer;
+
+    [Header("Attack Settings")]
+    [SerializeField] private GameObject turretBullet;
+    [SerializeField] private Transform spawnBullets;
+    [SerializeField] private float shootCooldown = 1f;
+
+    private RadiusDetectionSystem detectionSystem;
+    private Transform playerTarget;
     private float lastShootTime;
-    [SerializeField] private float shootCooldown;
 
     private void Awake()
     {
-        Initialize();
+        if (detectionPoint == null)
+        {
+            detectionPoint = transform;
+        }
+
+        detectionSystem = gameObject.AddComponent<RadiusDetectionSystem>();
+        detectionSystem.SetDetectionRadius(detectionRadius);
+        detectionSystem.SetTargetLayer(playerLayer);
     }
+
     void Update()
     {
         if (detectionSystem.DetectTarget())
         {
             playerTarget = detectionSystem.GetTarget();
-
             if (Time.time >= lastShootTime + shootCooldown)
             {
                 Attack();
                 lastShootTime = Time.time;
             }
         }
-
     }
 
-    private void Initialize()
-    {
-        if (detectionPoint == null)
-        {
-            detectionPoint = transform;
-        }
-            detectionSystem = new RadiusDetectionSystem(detectionPoint, detectionRadius, playerLayer);
-    }
     private void Attack()
     {
         Vector3 spawnPos = spawnBullets.transform.position;
         GameObject bullet = Instantiate(turretBullet, spawnPos, Quaternion.identity);
         TurretBullet bulletScript = bullet.GetComponent<TurretBullet>();
-        bulletScript.SetTarget(playerTarget);
-
+        if (bulletScript != null)
+        {
+            bulletScript.SetTarget(playerTarget);
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
-      
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(detectionPoint.position, detectionRadius);
+        if (detectionPoint != null)
+        {
+            Gizmos.DrawWireSphere(detectionPoint.position, detectionRadius);
+        }
+        else
+        {
+            Gizmos.DrawWireSphere(transform.position, detectionRadius);
+        }
     }
 }
