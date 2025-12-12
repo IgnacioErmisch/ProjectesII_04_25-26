4c2437434297f9ebd34cf83300df790c85e1b6d8
Adria Lama <adria.lama@enti.cat>
2025-12-12 11:41:31 +0100

    death-anim

--- Assets/Proyect/Scripts/Player/PlayerAnimatorController.cs
+++ Assets/Proyect/Scripts/Player/PlayerAnimatorController.cs
@@ -5,7 +5,8 @@ public class PlayerAnimatorController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private PlayerJump playerJump;
-   
+    [SerializeField] private PlayerCombatController playerCombatController;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -16,6 +17,7 @@ public class PlayerAnimatorController : MonoBehaviour
     {
         PlayWalkAnimation();
         PlayJumpAnimation();
+        PlayDeathAnimation();
     }
 
     private void PlayWalkAnimation()
@@ -41,5 +43,25 @@ public class PlayerAnimatorController : MonoBehaviour
         {
             animator.SetBool("isJumping", false);
         }
+    }
+
+    private void PlayDeathAnimation()
+    {
+        if (playerCombatController.GetCurrentHealth() <= 0)
+        {
+            animator.SetBool("isDeath", true);
+
+        }
+        else
+        {
+            animator.SetBool("isDeath", false);
+        }
+    }
+
+    public void ResetAnimations()
+    {
+        animator.SetBool("isDeath", false);
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isJumping", false);
     }
 }
