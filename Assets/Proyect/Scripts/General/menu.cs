8e9bd9bbfa737decab1f4b5f2737564ad9c5c296
Adria Lama <adria.lama@enti.cat>
2026-02-28 18:22:08 +0100

    menu-controller

--- Assets/Proyect/Scripts/General/menu.cs
+++ Assets/Proyect/Scripts/General/menu.cs
@@ -1,29 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class menuScene : MonoBehaviour
 {
-
     public GameObject panelSettings;
+    public GameObject primerBotonMenuPrincipal;
+    public GameObject sfx;
+
     void Start()
     {
         Slider musicSlider = GameObject.Find("Musica")?.GetComponent<Slider>();
-        Slider sfxSlider = GameObject.Find("SFX")?.GetComponent<Slider>(); 
+        Slider sfxSlider = GameObject.Find("SFX")?.GetComponent<Slider>();
     }
 
-  
     public void LoadSelector()
     {
         SceneManager.LoadScene("LevelSelector");
         Time.timeScale = 1;
     }
-
     public void ExitGame()
     {
         Time.timeScale = 1;
         Application.Quit();
-
     }
     public void LoadMenu()
     {
@@ -32,10 +32,14 @@ public class menuScene : MonoBehaviour
     public void Settings()
     {
         panelSettings.SetActive(true);
-    }
+        EventSystem.current.SetSelectedGameObject(sfx);
 
+    }
     public void closeSettings()
     {
         panelSettings.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(primerBotonMenuPrincipal);
     }
 }
